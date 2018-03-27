66ae263ed4a214e147ad22046106ff253aa97d50
Oskar <oskarradonkimball@gmail.com>
2018-03-27 12:02:51 -0700

    Add REAMDE, format index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       <p><?php the_title(); ?></p>
     <?php endwhile; else : ?>
-	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
+	    <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <?php endif; ?>
   </body>
 </html>
