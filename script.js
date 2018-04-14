c1cd36de2dc7e3f95b5f33d5e8ad51b60f5cdaca
Oskar Radon <oskarradonkimball@gmail.com>
2018-04-14 13:03:54 -0700

    Begin writing functions

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 $(function() {
   // var for # of projects
-  // depeneding on the number of projects
+
+  function changeSnake() {
+    let numOfProjects = $("main svg text textPath").length;
+    console.log(numOfProjects);
+
+    setOffset(numOfProjects);
+    setFontSize(numOfProjects);
+    setHeight(numOfProjects);
+    setMargins(numOfProjects);
+  }
+
+  function setOffset(x) {}
+
+  function setFontSize(x) {}
+
+  function setHeight(x) {}
+
+  function setMargins(x) {}
+
+  changeSnake();
+
+  // depending on the number of projects
   // set offset of textPath element
   // set font-size of .text-snake text element
   // set height of .text-snake element
