af54499655e69a33a004a91f8cd212b63f308d0c
Oskar Radon <oskarradonkimball@gmail.com>
2018-04-14 17:38:35 -0700

    Add in page title and more projects link

--- single.php
+++ single.php
@@ -11,7 +11,11 @@ get_header();
 ?>
 
 <main>
+	<h1 class="page-title"> <?php the_title() ?> </h1>
+
 	<?php the_content() ?>
+
+	<a href="/" class="more-projects-link">More projects...</a>
 </main>
 
 <?php get_footer(); ?>
