214e4c8e1a2adbf066b9e180abc1ae15ccef5f07
Oskar <oskarradonkimball@gmail.com>
2018-04-24 14:52:34 -0700

    Animation is complete!!

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 $(function() {
-	var dur = 5,
-		delayBetween = 0.2;
+	var dur = 100;
+	// 	repDelay = 5;
+	//
+	// TweenMax.to("textPath", dur, {
+	// 	attr: { startOffset: "100%" },
+	// 	ease: Sine.easeInOut,
+	// 	repeatDelay: repDelay,
+	// 	repeat: -1,
+	// 	yoyo: true
+	// });
 
-	TweenMax.staggerTo(
-		"textPath",
-		dur,
-		{
+	var tl = new TimelineMax({ repeat: -1 });
+	tl.add([
+		TweenMax.to(".text1", dur, {
 			attr: { startOffset: "100%" },
-			ease: Power1.easeOut,
-			repeat: -1
-		},
-		delayBetween
-	);
+			ease: Power0.easeNone
+		}),
+		TweenMax.to(".text2", dur, {
+			attr: { startOffset: "0%" },
+			ease: Power0.easeNone
+		})
+	]);
+	tl.play();
 });
