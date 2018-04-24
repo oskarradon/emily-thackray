801ece339304daa642a496ce2a3119b42c575a1f
Oskar <oskarradonkimball@gmail.com>
2018-04-24 10:49:46 -0700

    Add GSAP, begin animating

--- functions.php
+++ functions.php
@@ -21,6 +21,8 @@ function emily_thackray_scripts() {
 	wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', false, '1.8.1');
 	wp_enqueue_script('jquery');
 
+	wp_register_script('gsap', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js', false, '1.8.1');
+	wp_enqueue_script('gsap');
 
 	wp_register_script('myscript', get_template_directory_uri() .'/script.js', array ('jquery'), false, false);
 	wp_enqueue_script('myscript');
--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-$(function() {});
+$(function() {
+	var dur = 5,
+		delayBetween = 0.2;
+
+	TweenMax.staggerTo(
+		"textPath",
+		dur,
+		{
+			attr: { startOffset: "100%" },
+			ease: Power1.easeOut,
+			repeat: -1
+		},
+		delayBetween
+	);
+});
