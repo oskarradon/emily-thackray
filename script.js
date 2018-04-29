450844026720d098531379556c8ab01c47be30c5
Oskar <oskarradonkimball@gmail.com>
2018-04-28 21:32:23 -0700

    Scripts

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 $(function() {
-  var dur = 100;
-  // 	repDelay = 5;
-  //
-  // TweenMax.to("textPath", dur, {
-  // 	attr: { startOffset: "100%" },
-  // 	ease: Sine.easeInOut,
-  // 	repeatDelay: repDelay,
-  // 	repeat: -1,
-  // 	yoyo: true
-  // });
+	var dur = 100;
 
-  var tl = new TimelineMax({ repeat: -1 });
-  tl.add([
-    TweenMax.to(".text1", dur, {
-      attr: { startOffset: "100%" },
-      ease: Power0.easeNone
-    }),
-    TweenMax.to(".text2", dur, {
-      attr: { startOffset: "0%" },
-      ease: Power0.easeNone
-    })
-  ]);
-  tl.play();
+	var tl = new TimelineMax({ repeat: -1 });
+	tl.add([
+		TweenMax.to(".text1", dur, {
+			attr: { startOffset: "100%" },
+			ease: Power0.easeNone
+		}),
+		TweenMax.to(".text2", dur, {
+			attr: { startOffset: "0%" },
+			ease: Power0.easeNone
+		})
+	]);
+	tl.play();
 });
