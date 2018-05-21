21388b7aab428145eb4cc17577a3845465c2bb60
Oskar <oskarradonkimball@gmail.com>
2018-05-21 13:01:11 -0700

    Added bg img support and rm protected and private from titles

--- functions.php
+++ functions.php
@@ -10,8 +10,27 @@
 if ( ! function_exists( 'emily_thackray_setup' ) ) :
 	function emily_thackray_setup() {
 		add_theme_support( 'post-thumbnails' );
+		$defaults = array(
+			'default-color'          => '',
+			'default-image'          => '',
+			'default-repeat'         => 'no-repeat',
+			'default-position-x'     => 'left',
+      'default-position-y'     => 'top',
+      'default-size'           => 'cover',
+			'default-attachment'     => 'scroll',
+			'wp-head-callback'       => '_custom_background_cb',
+			'admin-head-callback'    => '',
+			'admin-preview-callback' => ''
+		);
+		add_theme_support( 'custom-background', $defaults );
+		function title_format($content) {
+			return '%s';
+		}
+		add_filter('private_title_format', 'title_format');
+		add_filter('protected_title_format', 'title_format');
 	}
 endif;
+
 add_action( 'after_setup_theme', 'emily_thackray_setup' );
 
 function emily_thackray_scripts() {
