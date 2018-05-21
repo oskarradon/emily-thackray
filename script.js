8880e21754e7c719e8084225bad10c54bb14ed6e
Oskar <oskarradonkimball@gmail.com>
2018-05-21 13:01:51 -0700

    Rm erroneous hide text2 js

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
 window.addEventListener("load", function() {
-	// hide second textPath element below a specific screen width
-	function hideText() {
-		var w = window,
-			d = document,
-			e = d.documentElement,
-			g = d.getElementsByTagName("body")[0],
-			x = w.innerWidth || e.clientWidth || g.clientWidth,
-			screenWidth = 768,
-			el = document.getElementsByClassName("text2")[0];
-
-		if (x <= screenWidth) {
-			el.style.display = "none";
-		} else {
-			el.style.display = "block";
-		}
-	}
-
-	hideText();
-
 	// animate textPath element
 	var dur = 100;
 
@@ -33,21 +14,4 @@ window.addEventListener("load", function() {
 		})
 	]);
 	tl.play();
-
-	// used to add an event listener
-	var addEvent = function(object, type, callback) {
-		if (object == null || typeof object == "undefined") return;
-		if (object.addEventListener) {
-			object.addEventListener(type, callback, false);
-		} else if (object.attachEvent) {
-			object.attachEvent("on" + type, callback);
-		} else {
-			object["on" + type] = callback;
-		}
-	};
-
-	// hides second textPath element if screen is resized
-	addEvent(window, "resize", function(event) {
-		hideText();
-	});
 });
